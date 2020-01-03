61394843340dfa5772a9915aa0a9103f3fd69143
Nazareth Rangel <naz7771313@gmail.com>
2020-01-03 12:55:04 -0600

    sufixes

--- root.py
+++ root.py
@@ -2,7 +2,7 @@ import csv
 from itertools import groupby
 
 arr = set()
-with open("word_list.csv") as f:
+with open("old_word_list.csv") as f:
     csv_reader = csv.reader(f, delimiter=',')
     for line in csv_reader:
         arr.add(line[0])
@@ -12,18 +12,18 @@ arr.sort()
 print len(arr)
 
 with open('sorted_word_list.csv', mode='w+') as f:
-    writer = csv.writer(f, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for x in arr:
         writer.writerow([x])
 
 with open('sufix_word_list.csv', mode='w+') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     
-    for k, x in groupby(arr, lambda y: y[:2] if len(y) > 1 else y[0]):
+    for k, x in groupby(arr, lambda y: y[:3] if len(y) > 1 else y[0]):
         writer.writerow(list(x))
 
     writer.writerow(['/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /'])
 
     arr.sort(key=lambda y: "".join(reversed(y)))
-    for k, x in groupby(arr, lambda y: y[-2:] if len(y) > 1 else y[0]):
+    for k, x in groupby(arr, lambda y: y[-3:] if len(y) > 1 else y[0]):
         writer.writerow(list(x))
