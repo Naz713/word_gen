1153566ecb9ea21596ece402f55af43e432b94f5
Nazareth Rangel <naz7771313@gmail.com>
2020-01-06 15:49:39 -0600

    commit

--- root.py
+++ root.py
@@ -1,6 +1,21 @@
 import csv
 from itertools import groupby
 
+
+def agrupator(word, length, prefix=True):
+    sufix = ''
+    counter = length
+    if not prefix:
+        word = "".join(reversed(word))
+    for letter in word:
+        sufix += letter
+        if letter != '\xce' and letter != '\xc4':
+            counter -= 1
+        if counter == 0:
+            return sufix if prefix else "".join(reversed(sufix))
+    return sufix if prefix else "".join(reversed(sufix))
+
+
 arr = set()
 with open("old_word_list.csv") as f:
     csv_reader = csv.reader(f, delimiter=',')
@@ -16,19 +31,38 @@ with open('sorted_word_list.csv', mode='w+') as f:
     for x in arr:
         writer.writerow([x])
 
-arr_root = reduce(lambda z, y: y.split(" ") + z, arr, [])
-arr = set(arr_root)
-arr = list(arr)
-arr.sort()
+length = 3
 
-with open('flex_root_list.csv', mode='w+') as f:
+with open('sufix_word_list.csv', mode='w+') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    
-    for k, x in groupby(arr, lambda y: y[:3] if len(y) > 1 else y[0]):
+
+    for k, x in groupby(arr, lambda y: agrupator(y, length)):
         writer.writerow(list(x))
 
     writer.writerow(['/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /'])
 
     arr.sort(key=lambda y: "".join(reversed(y)))
-    for k, x in groupby(arr, lambda y: y[-3:] if len(y) > 1 else y[0]):
+    for k, x in groupby(arr, lambda y: agrupator(y, length, False)):
         writer.writerow(list(x))
+
+length = 2
+
+arr_root = reduce(lambda z, y: y.split(" ") + z, arr, [])
+arr = set(arr_root)
+arr = list(arr)
+arr.sort()
+
+
+with open('flex_root_list.csv', mode='w+') as f:
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for k, x in groupby(arr, lambda y: agrupator(y, length)):
+        writer.writerow(list(x))
+
+    writer.writerow(['/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /'])
+
+    arr.sort(key=lambda y: "".join(reversed(y)))
+    for k, x in groupby(arr, lambda y: agrupator(y, length, False)):
+        writer.writerow(list(x))
+
+
